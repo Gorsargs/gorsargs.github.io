7d22fac9664cdcf926093640bbcd9ca35582a37c
unknown <gorsargsyanm@gmail.com>
2023-09-06 05:22:12 +0400

    add game loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+let canvas
+let context
 
+window.onload = init
+
+function init() {
+  canvas = document.getElementById('tile_game')
+  context = canvas.getContext('2d')
+  // Start the first frame request
+  window.requestAnimationFrame(gameLoop)
+}
+
+function gameLoop() {
+  // code here
+  window.requestAnimationFrame(gameLoop)
+}
