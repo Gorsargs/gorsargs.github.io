63a2500ac702f4e5b6506e804fc25ed66def15af
unknown <gorsargsyanm@gmail.com>
2023-09-25 10:31:03 +0400

    refactor GameDataSingleton

--- src/GameData/GameDataSingleton.js
+++ src/GameData/GameDataSingleton.js
@@ -1,5 +1,20 @@
 class GameDataSingleton {
   constructor() {
+    this.level
+
+    this.moves
+    this.pointsToReach
+    this.currentPoints
+
+    this.resetBoard
+    this.radiusRemove
+    this.teleportTiles
+    this.pause
+    this.gameOver
+    this._resetToDefault()
+  }
+
+  _resetToDefault() {
     this.level = 0
 
     this.moves = 30
@@ -10,10 +25,34 @@ class GameDataSingleton {
     this.radiusRemove = 3
     this.teleportTiles = 10
     this.pause = false
+    this.gameOver = false
   }
+
+  restart() {
+    this._resetToDefault()
+  }
+
+  onWin() {
+    this.level++
+    this.moves = 30
+    this.currentPoints = 0
+    this.pointsToReach += 30
+    this.resetBoard = 3
+    this.radiusRemove = 3
+    this.teleportTiles = 10
+  }
+
   onMove() {
     if (this.moves > 0) {
       this.moves--
+    }
+  }
+
+  onTileDelete(deletedTilesCount) {
+    if (deletedTilesCount < 3) {
+      this.currentPoints += deletedTilesCount * 5
+    } else {
+      this.currentPoints += deletedTilesCount * 10
     }
   }
 
